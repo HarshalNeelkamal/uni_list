d0872392ca5feb4f2cf591b572c5da7daf653f45
MobilityHarshal <harshalneel@gmail.com>
2018-08-05 20:53:35 -0400

    Add, Delete, Get implemented

--- api/resources/user-resource.js
+++ api/resources/user-resource.js
@@ -3,21 +3,43 @@ var router = express.Router();
 var manager = require('../managers/user-manager');
 
 router.get("/:user_id",(req, res, next) => {
-  manager.handleGet(req.params.user_id, result => {
-    res.send(result[0]);
+  manager.handleGet(req.params.user_id, (error, result) => {
+    if(error){
+      res.status(500).json({
+        message: error
+      })
+    } else {
+      res.send(result);
+    }
   });
 });
 
 router.post("/",(req, res, next) => {
-
+  manager.handlePost(req.body, (error, result) => {
+    if(error){
+      res.status(500).json({
+        message: error
+      })
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 router.patch("/",(req, res, next) => {
 
 });
 
-router.delete("/",(req, res, next) => {
-
+router.delete("/:user_id",(req, res, next) => {
+  manager.handlePost(req.params.user_id, (error, result) => {
+    if(error){
+      res.status(500).json({
+        message: error
+      })
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 module.exports = router;
--- api/util/connection.js
+++ api/util/connection.js
@@ -4,11 +4,11 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 var pool = mysql.createPool({
-    host     : process.env.HOST,
-    user     : process.env.user,
-    password : process.env.password,
-    database : process.env.database,
-    port     : process.env.port,
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB,
+    port     : process.env.DB_PORT,
 });
 
 module.exports = {
