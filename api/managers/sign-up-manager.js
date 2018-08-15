0190525ad22b43ba0453df8c9edc8f16b006bac4
MobilityHarshal <harshalneel@gmail.com>
2018-08-15 00:19:10 -0400

    In progress

---
+++ api/managers/sign-up-manager.js
@@ -0,0 +1,15 @@
+var emailExistence = require('email-existence');
+
+module.exports = {
+
+  signupUser = function(json_body, callback){
+    emailExistence.check(json_body.email, (error, response) => {
+      if(error){
+        callback(error);
+      } else {
+        callback(null, response);
+      }
+    });
+  },
+  
+};
---
+++ api/resources/sign-up-resource.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var router = express.Router();
+var signup_manager = require('../resources/sign-ip-manager');
+
+router.post("/", (req, res, next) => {
+  signup_manager.signupUser(req.body, (error, response) => {
+    
+  });
+})
